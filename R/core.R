f8a296850cce9b74a6340484c7af1235ec00fcd9
systats <nomis.roth@gmx.net>
2019-05-22 01:51:47 +0200

    added pagination

--- R/core.R
+++ R/core.R
@@ -1,16 +1,61 @@
-#' @export  
-make_request <- function(end_point, dev = F){
+#' @export 
+make_request <- function(end_point, page = NULL, leagues = NULL, includes = NULL){
   
   base_url <- "https://soccer.sportmonks.com/api/v2.0/"
-  q <- glue::glue("{base_url}{end_point}?api_token={Sys.getenv('sportmonks')}")
+  page <- ifelse(is.null(page), "", glue::glue("&page={page}"))
+  leagues <- ifelse(is.null(leagues), "", glue::glue("&leagues={paste(leagues, collapse = ',')}"))
+  includes <- ifelse(is.null(includes), "", glue::glue("&include={paste(includes, collapse = ',')}"))
+  q <- glue::glue("{base_url}{end_point}?api_token={Sys.getenv('sportmonks')}{includes}{page}{leagues}") #&include={includes}
+  
   res <- httr::GET(q)
   
-  if(dev) {
-    print(q) 
-    print(res)
+  if(http_error(res)){
+    main <- httr::message_for_status(res$status_code)
+    log_error(header = res$status_code, info = main)
+  } else {
+    #data_len <- content(res)$data %>% length()
+    main <- httr::message_for_status(res$status_code)
+    log_success(header = res$status_code, info = main)
   }
   
-  return(httr::content(res)$data)
+  return(res)
+}
+
+
+#' @export  
+request <- function(end_point, dev = F, pages = T, ...){
+  
+  param <- list(...)
+  
+  res <- make_request(end_point, leagues = param[["leagues"]], includes = param[["includes"]])
+  ct <- httr::content(res)
+  data <- ct$data
+  meta <- ct$meta
+  
+  if(dev) {
+    print(q)
+    print(res)
+    print(meta)
+  }
+  
+  if(length(meta$pagination$total_pages) > 0){
+    if(pages & meta$pagination$total_pages > 1){
+      
+      data <- 2:meta$pagination$total_pages %>%
+        imap(~{
+          print(.x)
+          end_point %>% 
+            make_request(page = .x, leagues = param[["leagues"]], includes = param[["includes"]]) %>%
+            httr::content(.) %>%
+            .$data
+        }) %>%
+        c(list(data), .) %>% 
+        unlist(.,recursive = F)
+      
+    }
+  }
+
+  return(data)
 }
 
 #' @export 
@@ -25,10 +70,16 @@ parse_request <- function(d){
   }
 }
 
+
 #' @export 
-request <- function(end_point, dev = F){
-  end_point %>% 
-    make_request() %>%
-    parse_request() %>% 
-    janitor::clean_names()
+log_success <- function(header = "", info = ""){
+  info <- as.character(info) %>% paste0(., "\n") 
+  crayon::green(glue::glue("Success {header} [{Sys.time()}]: ")) %+% info %>% cat()
 }
+
+#' @export 
+log_error <- function(header = "", info = ""){
+  info <- as.character(info) %>% paste0(., "\n")
+  crayon::red(glue::glue("Error {header} [{Sys.time()}]: ")) %+% info %>% cat()
+}
+
--- R/endpoints.R
+++ R/endpoints.R
@@ -1,139 +1,141 @@
 #' @export 
-get_continents <- function() request("continents")
+get_continents <- function(...) request("continents", ...)
 
 #' @export 
-get_continent <- function(id){
-  glue::glue("continents/{id}") %>% request()
+get_continent <- function(id, ...){
+  glue::glue("continents/{id}") %>% request(...)
 }
 
 #' @export 
-get_countries <- function() request("countries")
+get_countries <- function(...) request("countries", ...)
 
 #' @export 
-get_country <- function(id){
-  glue::glue("continents/{id}") %>% request()
+get_country <- function(id, ...){
+  glue::glue("countries/{id}") %>% request(...)
 }
 
 #' @export 
-get_leagues <- function() request("leagues")
+get_leagues <- function(...) request("leagues", ...)
 
 #' @export 
-get_league <- function(id){
-  glue::glue("leagues/{id}") %>% request()
+get_league <- function(id, ...){
+  glue::glue("leagues/{id}") %>% request(...)
 }
 
 #' @export 
-get_seasons <- function() request("seasons")
+get_seasons <- function(...) request("seasons", ...)
 
 #' @export 
-get_season <- function(id){
-  glue::glue("seasons/{id}") %>% request()
+get_season <- function(id, ...){
+  glue::glue("seasons/{id}") %>% request(...)
 }
 
 #' @export 
-get_fixture <- function(id = NULL, date = NULL){
+get_fixture <- function(id = NULL, date = NULL, ...){
   if(!is.null(id)) ep <- glue::glue("fixtures/{id}")
   if(!is.null(date)) ep <- glue::glue("fixtures/date/{date}")
-  ep %>% request()
+  ep %>% request(...)
 }
 
 #' @export 
-get_fixtures <- function(ids = NULL, dates = NULL){
+get_fixtures <- function(ids = NULL, dates = NULL, year = NULL, ...){
+  
   if(!is.null(ids)) ep <- ids %>% paste(collapse = ",") %>% paste("fixtures/multi/", .) 
   if(!is.null(dates)) ep <- dates[1:2] %>% paste(collapse = "/") %>% paste0("fixtures/between/", .) 
-  ep %>% request()
+  if(!is.null(year)) ep <- glue::glue("fixtures/between/{year}-01-01/{year+1}-01-01")
+  ep %>% request(...)
 }
 
 #' @export 
-get_live <- function() request("livescores")
+get_live <- function(...) request("livescores", ...)
 #get_live_now <- function() make_request("livescores/now")
 
 #' @export 
-get_comments <- function(game_id){
-  glue::glue("commentaries/fixture/{game_id}") %>% request()
+get_comments <- function(game_id, ...){
+  glue::glue("commentaries/fixture/{game_id}") %>% request(...)
 }
 
 #' @export 
-get_highlights <- function(game_id){
-  glue::glue("commentaries/fixture/{game_id}") %>% request()
+get_highlights <- function(game_id, ...){
+  glue::glue("commentaries/fixture/{game_id}") %>% request(...)
 }
 
 #' @export 
-get_head2head <- function(team1, team2){
-  glue::glue("head2head/{team1}/{team2}") %>% request()
+get_head2head <- function(team1, team2, ...){
+  glue::glue("head2head/{team1}/{team2}") %>% request(...)
 }
 
 #' @export 
-get_head2head <- function(team1, team2){
-  glue::glue("head2head/{team1}/{team2}") %>% request()
+get_head2head <- function(team1, team2, ...){
+  glue::glue("head2head/{team1}/{team2}") %>% request(...)
 }
 
 #' @export 
-get_team <- function(id, season_id = NULL){
+get_team <- function(id, season_id = NULL, ...){
   if(!is.null(season_id)) id <- paste0("season/", season_id)
-  glue::glue("teams/{id}") %>% request()
+  glue::glue("teams/{id}") %>% request(...)
 }
 
 #' @export 
-get_player <- function(id){
-  glue::glue("players/{id}") %>% request()
+get_player <- function(id, ...){
+  glue::glue("players/{id}") %>% request(...)
 }
 
 #' @export 
-get_topscores <- function(season_id, agg = F){
+get_topscores <- function(season_id, agg = F, ...){
   agg <- ifelse(agg, "/aggregated", "")
-  glue::glue("topscorers/season/{season_id}{agg}") %>% request()
+  glue::glue("topscorers/season/{season_id}{agg}") %>% request(...)
 }
 
 #' @export 
-get_venue <- function(id){
-  glue::glue("venues/{id}") %>% request()
+get_venue <- function(id, ...){
+  glue::glue("venues/{id}") %>% request(...)
 }
 
 #' @export 
 get_round <- function(round_id = NULL, season_id = NULL){
   if(!is.null(season_id)) round_id <- paste0("season/", season_id)
-  glue::glue("rounds/{round_id}") %>% request()
+  glue::glue("rounds/{round_id}") %>% request(...)
 }
 
 #' @export 
-get_odds <- function(game_id){
-  glue::glue("odds/fixture/{game_id}") %>% request()
+get_odds <- function(game_id, ...){
+  glue::glue("odds/fixture/{game_id}") %>% request(...)
 }
 
 #' @export 
-get_coache <- function(id){
-  glue::glue("coaches/{id}") %>% request()
+get_coache <- function(id, ...){
+  glue::glue("coaches/{id}") %>% request(...)
 }
 
 #' @export 
-get_stage <- function(stage_id = NULL, season_id = NULL){
+get_stage <- function(stage_id = NULL, season_id = NULL, ...){
   if(!is.null(season_id)) stage_id <- paste0("season/", season_id)
-  glue::glue("stages/{stage_id}") %>% request()
+  glue::glue("stages/{stage_id}") %>% request(...)
 }
 
 #' @export 
-get_bookmakers <- function() request("bookmakers")
+get_bookmakers <- function(...) request("bookmakers")
 
 #' @export 
 get_bookmaker <- function(book_id){
-  glue::glue("bookmakers/{book_id}") %>% request()
+  glue::glue("bookmakers/{book_id}") %>% request(...)
 }
 
 #' @export 
-get_markets <- function() request("markets")
+get_markets <- function(...) request("markets", ...)
 
 #' @export 
-get_market <- function(id){
-  glue::glue("markets/{id}") %>% request()
+get_market <- function(id, ...){
+  glue::glue("markets/{id}") %>% request(...)
 }
 
 #' @export 
-get_team_squads <- function(season_id = NULL, team_id = NULL){
-  glue::glue("squad/season/{season_id}/team/{team_id}") %>% request()
+get_team_squads <- function(season_id = NULL, team_id = NULL, ...){
+  glue::glue("squad/season/{season_id}/team/{team_id}") %>% request(...)
 }
 
 #' @export 
-get_tv <- function(game_id){
-  glue::glue("tvstations/fixture/{game_id}") %>% request()
+get_tv <- function(game_id, ...){
+  glue::glue("tvstations/fixture/{game_id}") %>% request(...)
 }
